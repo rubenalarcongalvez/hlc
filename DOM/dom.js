cda3fcc5d85e340df67e95eab5ed9576353a20a8
dieguxo91 <diegogarcia0512@gmail.com>
2023-02-14 14:02:46 +0100

    dom

--- DOM/dom.js
+++ DOM/dom.js
@@ -2,9 +2,3 @@ let cajita = document.createElement("input");
 
 let accion = document.querySelector("#accion");
 
-
-accion.appendChild(cajita);
-
-if(accion.firstChild()){
-    accion.removeChild(accion.firstChild);
-}
