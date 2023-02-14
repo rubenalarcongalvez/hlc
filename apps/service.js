35d257d98e026c2e81b747c2e9bd3914f4e35a24
Rubén Alarcón Gálvez <rubenalarcongalvez@gmail.com>
2023-02-14 16:13:27 +0100

    - Eliminación de cosas innecesarias.
    - Añadido funcionamiento de botones con estilos y active correctamente
    
    - Footer y perfilar HTML

--- apps/service.js
+++ apps/service.js
@@ -1,22 +1,69 @@
-function crearInput(){
-    let cajita = document.createElement("input");
+function crearInput() {
+    let cajita = document.createElement('input');
     return cajita;
 }
 
-function insertarEnNodo(div, input){
+function insertarEnNodo(div, input) {
     div.appendChild(input);
 }
 
-function crearTriangulo(){
-    let accion = document.querySelector("#accion");
+function activar(eleccion) {
+    let triangulo = document.querySelector('#triangulo');
+    let rectangulo = document.querySelector('#rectangulo');
+    let circulo = document.querySelector('#circulo');
+    let pentagono = document.querySelector('#pentagono');
+    let cuadrado = document.querySelector('#cuadrado');
+    let hexagono = document.querySelector('#hexagono');
 
-    while(accion.firstChild){
+    /* reiniciamos los active */
+    triangulo.className = 'nav-link';
+    rectangulo.className = 'nav-link';
+    circulo.className = 'nav-link';
+    pentagono.className = 'nav-link';
+    cuadrado.className = 'nav-link';
+    hexagono.className = 'nav-link';
+
+    switch (eleccion) {
+        case 'triangulo':
+            triangulo.className = 'nav-link active text-bg-primary';
+            break;
+
+        case 'rectangulo':
+            rectangulo.className = 'nav-link active text-bg-primary';
+            break;
+
+        case 'circulo':
+            circulo.className = 'nav-link active text-bg-primary';
+            break;
+
+        case 'pentagono':
+            pentagono.className = 'nav-link active text-bg-primary';
+            break;
+
+        case 'cuadrado':
+            cuadrado.className = 'nav-link active text-bg-primary';
+            break;
+
+        case 'hexagono':
+            hexagono.className = 'nav-link active text-bg-primary';
+            break;
+
+        default:
+            break;
+    }
+}
+
+function crearTriangulo() {
+    let accion = document.querySelector('#accion');
+
+    while (accion.firstChild) {
         accion.removeChild(accion.firstChild);
     }
+
     let cajita1 = crearInput();
     let cajita2 = crearInput();
 
-    cajita1.className="estiloTriangulo";
+    cajita1.className = 'estiloTriangulo';
 
     accion.appendChild(cajita1);
     accion.appendChild(cajita2);
